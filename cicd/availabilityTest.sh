dc70cb78f679e44b0b599a83c0aef39b2d3f551c
mariusbrandorff <144017807+mariusbrandorff@users.noreply.github.com>
2025-08-14 18:12:38 +0200

    Fixed availabilityTest.sh
    multiple typos

--- cicd/availabilityTest.sh
+++ cicd/availabilityTest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-ADDRESS=$(kubectl --kubeconfig kubeconfig get node -o yaml | grep address: | head -1 | cut -d: -f2 | tr -d ' ')
+ADDRESS=$(kubectl --kubeconfig kubeconfig get node -o yaml | grep address: | head -2 | tail -1 | cut -d: -f2 | tr -d ' ')
 PORT=$(kubectl --kubeconfig kubeconfig get svc | grep frontend | cut -d: -f2 - | cut -d/ -f1 -)
-URL=URL="$ADDRESS$PORT"
+URL="$ADDRESS:$PORT"
 RESULT=$(curl -o /dev/null -s -w "%{http_code}\n" $URL)
 if [ $RESULT = 200 ]; then
     echo "Successfully connected"
