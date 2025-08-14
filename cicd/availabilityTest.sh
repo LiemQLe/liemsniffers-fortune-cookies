34d8fdb084e5db3a2cc50a99a4673ddb8bf0d482
mariusbrandorff <144017807+mariusbrandorff@users.noreply.github.com>
2025-08-14 18:17:13 +0200

    Created test script for cicd (#36)
    Created a availabilityTest script
    
    Issue: #8

---
+++ cicd/availabilityTest.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+ADDRESS=$(kubectl --kubeconfig kubeconfig get node -o yaml | grep address: | head -2 | tail -1 | cut -d: -f2 | tr -d ' ')
+PORT=$(kubectl --kubeconfig kubeconfig get svc | grep frontend | cut -d: -f2 - | cut -d/ -f1 -)
+URL="$ADDRESS:$PORT"
+RESULT=$(curl -o /dev/null -s -w "%{http_code}\n" $URL)
+if [ $RESULT = 200 ]; then
+    echo "Successfully connected"
+    exit 0
+else
+    echo "Failed to connect to url"
+    exit 1
+fi
